4bf76bc8676037dd4bd2b67b74fb2869ec369679
ibrahim gaber <ibrahimgaber190086@gmail.com>
2023-05-31 04:56:45 +0300

    change logo size

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,7 +1,12 @@
 import { SiAmazongames as AppLogo } from "react-icons/si";
+import { Link } from "react-router-dom";
 
 const Logo = () => {
-  return <AppLogo size={"3em"} />;
+  return (
+    <Link to='/'>
+      <AppLogo size={"3em"} />
+    </Link>
+  );
 };
 
 export default Logo;
