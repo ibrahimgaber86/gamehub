54597286e57af4fdd2a2c676be8e4e864e55185e
ibrahim gaber <ibrahimgaber190086@gmail.com>
2023-06-06 11:33:42 +0300

    change scroll up button to affix component

--- src/components/ScrollUpBTN.tsx
+++ src/components/ScrollUpBTN.tsx
@@ -1,28 +1,32 @@
 import { FaArrowUp as Up } from "react-icons/fa";
 
-import { ActionIcon } from "@mantine/core";
+import { ActionIcon, Affix, Transition, rem } from "@mantine/core";
 import { useWindowScroll } from "@mantine/hooks";
 
 const ScrollUpBTN = () => {
   const [position, scrollTo] = useWindowScroll();
 
   return (
-    <ActionIcon
-      variant='filled'
-      color='primary'
-      radius={"xl"}
-      size={"xl"}
-      sx={{
-        transform: `translatey(${position.y < 200 ? "5rem" : "0px"})`,
-        position: "fixed",
-        bottom: "1rem",
-        right: "min(2rem , 10vw)",
-        transition: "all 300ms",
-      }}
-      onClick={() => scrollTo({ y: 0 })}
-    >
-      <Up />
-    </ActionIcon>
+    <Affix position={{ bottom: rem(20), right: rem(20) }}>
+      <Transition
+        transition={"slide-up"}
+        mounted={position.y > 200}
+        keepMounted
+      >
+        {(styles) => (
+          <ActionIcon
+            style={{ ...styles }}
+            variant='filled'
+            color='primary'
+            radius={"xl"}
+            size={"xl"}
+            onClick={() => scrollTo({ y: 0 })}
+          >
+            <Up />
+          </ActionIcon>
+        )}
+      </Transition>
+    </Affix>
   );
 };
 
